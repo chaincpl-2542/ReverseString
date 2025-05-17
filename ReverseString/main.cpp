42c0ed2447e345ad1db9ee8a80637da3e4d47642
chaincpl-2542 <chaincpl2542@gmail.com>
2025-05-17 09:33:16 +0700

    update

--- ReverseString/main.cpp
+++ ReverseString/main.cpp
@@ -1,21 +1,27 @@
 #include <iostream>
 #include <cstring>
+#include <cassert>
 
 using namespace std;
 
 void main() {
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
     char input[30];
 
     while (true)
     {
+        cout << "----------------------------------------------------" << endl;
+        cout << "Develop by Peeranat Luangaram 6738228" << endl;
+        cout << "----------------------------------------------------" << endl;
         cout << "Enter a Text But less than 30 characters: " << endl;
         cin.getline(input, 30);
 
-        if (cin.fail()) 
+        if (cin.fail())
         {
             cin.clear();
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
             cout << "Input too long. Please enter less than 30 characters.\n";
+            cout << "----------------------------------------------------" << endl;
             continue;
         }
 
@@ -31,5 +37,8 @@ void main() {
         cout << endl;
 
         cout << "------------------------------" << endl;
+
+        assert(_CrtCheckMemory());
+        _CrtDumpMemoryLeaks();
     }
 }
